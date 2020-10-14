357a19206d1eba635e7bfe20c285f2fafca19456
Michael S. Rizkalla <33562048+MichaelRizkalla@users.noreply.github.com>
2020-10-14 15:28:07 +0100

    correctly place __cpp_lib_constexpr_dynamic_alloc and add feature test

--- stl/inc/yvals_core.h
+++ stl/inc/yvals_core.h
@@ -1173,8 +1173,13 @@
 #define __cpp_lib_concepts 201907L
 #endif // !defined(__EDG__) || defined(__INTELLISENSE__)
 
-#define __cpp_lib_constexpr_algorithms  201806L
-#define __cpp_lib_constexpr_complex     201711L
+#define __cpp_lib_constexpr_algorithms 201806L
+#define __cpp_lib_constexpr_complex    201711L
+
+#if _HAS_CXX20 && defined(__cpp_constexpr_dynamic_alloc)
+#define __cpp_lib_constexpr_dynamic_alloc 201907L
+#endif
+
 #define __cpp_lib_constexpr_functional  201907L
 #define __cpp_lib_constexpr_iterator    201811L
 #define __cpp_lib_constexpr_memory      201811L
@@ -1215,7 +1220,6 @@
 #define __cpp_lib_to_array                     201907L
 #define __cpp_lib_type_identity                201806L
 #define __cpp_lib_unwrap_ref                   201811L
-#define __cpp_lib_constexpr_dynamic_alloc      201907L
 #endif // _HAS_CXX20
 
 #ifndef _M_CEE
